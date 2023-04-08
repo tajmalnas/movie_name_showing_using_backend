540a8b7885067b613070c6d250e7431784045a6d
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-04-09 00:14:35 +0530

    used SWAPI api to get name of Star war movies

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+
+  const [movies,setMovies] = useState([])
+
+  function fetchMovieHandler(){
+    fetch('https://swapi.py4e.com/api/films/').then(response=>{
+      return response.json();
+    }).then(data=>{
+      const transformMovies = data.results.map(movieData=>{
+        return {
+          id:movieData.episode_id,
+          title:movieData.title,
+          openingText:movieData.opening_crawl,
+          releaseData:movieData.release_data
+        };
+
+      })
+      setMovies(transformMovies);
+    })
+
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
